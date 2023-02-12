8e85f95a565c3f234761b54fc5d07019ed5f2a9a
Lisa Anderson <lisachananderson@gmail.com>
2023-02-11 17:44:16 -0800

    init commit

---
+++ js/script.js
@@ -0,0 +1,191 @@
+/*
+    COMP 690 Assignment 1
+    Lisa Anderson
+    02/15/2023
+*/
+
+// STEP 1
+
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+*/
+
+// STEP 2
+
+/*
+2975    // numeric literal
+'Lisa'  // string literal
+true    // boolean literal
+null    // null literal
+*/
+
+// STEP 3
+
+/*
+let sum = 1 + 2;
+let perimeter = 2 * (length + width);
+*/
+
+// STEP 4
+
+/*
+let strFirstName;
+var lastName;
+let strAddress;
+var city;
+let strState;
+var zipCode;
+let intYourAge;
+var referralSource;
+let blnMayWeContactYou;
+*/
+
+// STEP 5
+
+/*
+let strFirstName = 'Lisa';
+let lastName;
+lastName = 'Anderson';
+let city = 'San Diego', strState = 'California';
+*/
+
+// STEP 6
+
+/*
+let msg = 2 + ' boys';
+console.log(msg);
+*/
+
+// STEP 7
+
+/*
+let msg1 = true + ' fact';
+console.log(msg1);
+let msg2 = 10 + true;
+console.log(msg2);
+*/
+
+// STEP 8
+
+/*
+String is not valid because the apostrophe in "I'm" is not escaped and is mistaken for an end-of-string terminator, causing problems when there is additional content beyond it.  To escape the character, you must precede it with a backslash (\).
+
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+*/
+
+// STEP 9
+
+/*
+let nullVal = null;
+console.log(nullVal);
+let undefinedVal;
+console.log(undefinedVal);
+*/
+
+// STEP 10
+
+/*
+console.log(typeof 'Lisa');
+console.log(typeof 2975);
+console.log(typeof true);
+console.log(typeof [1, 2, 3]);
+console.log(typeof undefinedVar);
+*/
+
+// STEP 11
+
+/*
+alert('Hello ' + 'Lisa Anderson' + ', welcome to the JavaScript class!');
+*/
+
+// STEP 12
+
+/*
+let name = 'Lisa Anderson';
+alert('Hello ' + name + ', welcome to the JavaScript class!');
+*/
+
+// STEP 13
+
+/*
+let name = 'Lisa Anderson';
+let course = 'JavaScript';
+alert('Hello ' + name + ', welcome to the ' + course + ' class!');
+*/
+
+// STEP 14
+
+/*
+let name = 'Lisa Anderson';
+let course = 'JavaScript';
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+*/
+
+// STEP 15
+
+/*
+let name = prompt('Please enter name:');
+let course = 'JavaScript';
+if (name) {
+    alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+}
+*/
+
+// STEP 16
+
+/*
+let name = 'Lisa Anderson';
+let course = prompt('Please enter course:');
+if (course) {
+    alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+}
+*/
+
+// STEP 17
+
+/*
+let x = 10;
+let y = 20;
+console.log(x + y);
+*/
+
+// STEP 18
+
+/*
+let x = 20;
+x += 20;
+console.log(x);
+*/
+
+// STEP 19
+
+/*
+let x = 20;
+x *= 5;
+console.log(x);
+*/
+
+// STEP 20
+
+/*
+let x = 20 % 3;
+x /= 1;
+console.log(x);
+*/
+
+// STEP 21
+
+/*
+let x = 2, y = 3;
+console.log(x > 0 && y <= 4);
+*/
+
+// STEP 22
+
+let x = 2, y = 3;
+console.log(x != 2 || y === '3');
